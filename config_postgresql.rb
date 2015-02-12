0c89b0669f0862f44ddc2c5dd9ec3a65f0905a0f
damien clochard <damien@dalibo.info>
2015-02-12 11:19:08 +0100

    Create config_postgresql.rb

---
+++ config_postgresql.rb
@@ -0,0 +1,21 @@
+# PostgreSQL Dashboard config file
+{
+  # Connection
+  'host' => '',
+  'port' => '5432',
+  'options' => '',  # Backend options 
+  'tty'  => '',     # Obsolete
+  'dbname' => '',
+  'user'   => '',
+  'password' => '',
+
+  # Scheduler
+  'freq' => '10s',
+
+  # Twitter
+  'tw_consumer_key'= 'YOUR_CONSUMER_KEY'
+  'tw_consumer_secret' = 'YOUR_CONSUMER_SECRET'
+  'tw_config.access_token' = 'YOUR_OAUTH_TOKEN'
+  'tw_access_token_secret' = 'YOUR_OAUTH_SECRET'
+
+}
